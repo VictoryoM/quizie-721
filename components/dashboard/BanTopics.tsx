0e527394dcc1bd44de84d1c917ce9de415d20f8f
MisanduPerera <misandusas@gmail.com>
2023-05-10 15:53:24 +1200

    added ban topic section
    footer issue fixed

--- components/dashboard/BanTopics.tsx
+++ components/dashboard/BanTopics.tsx
@@ -1,13 +1,31 @@
-import { Box, Heading } from '@chakra-ui/react'
-import React from 'react'
+import { Box, Heading, Input, Button } from '@chakra-ui/react'
+import React, { useState } from 'react'
 
 const BanTopics = () => {
+
+  const [topic, setTopic] = useState("");
+
+  const handleTopicChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    setTopic(event.target.value);
+  };
+
+  const handleBanTopic = () => {
+    // implement topic banning logic here
+    console.log(`Banning topic: ${topic}`);
+  };
+
   return (
     <div>
-      <Box mb={'40'}>
-        <Heading textAlign={'center'}>
-          Ban Topics
-        </Heading>
+      <Box mb={20}>
+        <Box mb={10}>
+          <Heading textAlign={'center'}>
+            Ban Topics
+          </Heading>
+        </Box>
+        <Box w={['100%', '80%']} maxW='750px' mx='auto'>
+          <Input placeholder='Enter topic to ban' value={topic} onChange={handleTopicChange} w='70%' mr={4} />
+          <Button colorScheme='red' onClick={handleBanTopic} w='20%'>Ban</Button>
+        </Box>
       </Box>
     </div>
   )
